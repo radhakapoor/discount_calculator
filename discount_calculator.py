0d033570b1b9d709b04d79e69d1f9a6b8d47dfa9
radha kapoor <radha@radhakapoor.net>
2014-03-13 16:38:07 -0400

    class of discount calculator

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,22 +1,4 @@
-import sys
-
-
-def discount_amount(total_amount, discount, discount_type):
-    if discount_type == "percentage":
-        discount_amount = total_amount * discount
-        print discount_amount
+class DiscountCalculator(object):
     
-    if discount_type == "dollar":
-        discount_amount = total_amount - discount         
-        print discount_amount 
- 
-    
-def main():
-    total_amount = float(sys.argv[1])
-    discount = float(sys.argv[2])
-    discount_type = sys.argv[3]
-    discount_amount(total_amount, discount, discount_type)
-    
-    
-if __name__ == '__main__':
-    main()
+    def calculate(self, total, discount_amount, discount_type):
+        return 10.0
