ac276779bf7936c3de27232b6dbe04f125340ec2
radha kapoor <radha@radhakapoor.net>
2014-03-14 12:01:16 -0400

    changing the discount amount

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,4 +1,4 @@
 class DiscountCalculator(object):
     
     def calculate(self, total, discount_amount, discount_type):
-        return 10.0
+        return 20.0
--- test_discount_calculator.py
+++ test_discount_calculator.py
@@ -2,9 +2,9 @@ import unittest
 from discount_calculator import DiscountCalculator
 
 class DiscountCalculatorTests(unittest.TestCase):
-    def test_ten_percent_discount(self):
+    def test_twenty_percent_discount(self):
         discount_calculator = DiscountCalculator()
-        result = discount_calculator.calculate(100,10, 'percent')
-        self.assertEqual(10.0, result)
+        result = discount_calculator.calculate(100, 20, 'percent')
+        self.assertEqual(20.0, result)
         
         
